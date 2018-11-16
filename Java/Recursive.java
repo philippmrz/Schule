a6bfaa916cffc08c867b41d261678acb0c26bd95
Philipp Merz <phmrch@gmail.com>
2018-11-16 16:12:36 +0100

    Added recursive sequence calculation

---
+++ Java/Recursive.java
@@ -0,0 +1,12 @@
+class Recursive {
+  public static void main(String[] args) {
+    System.out.println(recurse(3));
+  }
+
+  static int recurse(int n) {
+    if (n == 0) return 1;
+    else {
+      return 2 * recurse(n - 1);
+    }
+  }
+}
