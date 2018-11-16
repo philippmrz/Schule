b5c1434b51c6631e92b5c6352bc65548cb796b35
Philipp Merz <phmrch@gmail.com>
2018-11-16 16:27:12 +0100

    Added pascal triangle function

--- Java/Recursive.java
+++ Java/Recursive.java
@@ -1,12 +1,26 @@
 class Recursive {
   public static void main(String[] args) {
-    System.out.println(recurse(3));
+    // System.out.println(sequence(3));
+    System.out.println(pascalTriangle(1, 2));
   }
 
   static int sequence(int n) {
     if (n == 0) return 1;
     else {
-      return 2 * recurse(n - 1);
+      return 2 * sequence(n - 1);
     }
+  }
+
+  static int pascalTriangle(int row, int col) {
+    if (row == col || col <= 1) return 1;
+    else if (col > row) {
+      printInvalidInput();
+      return 0;
+    }
+    else return pascalTriangle(row - 1, col) + pascalTriangle(row - 1, col - 1);
+  }
+
+  static void printInvalidInput() {
+    System.out.println("Row can't be larger than column");
   }
 }
