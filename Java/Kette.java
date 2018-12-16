1e859137f96e5ecc42d0c637061727be03eda021
Philipp Merz <phmrch@gmail.com>
2018-12-16 19:51:33 +0100

    Beautufied

--- Java/Kette.java
+++ Java/Kette.java
@@ -10,64 +10,64 @@ public class Kette {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int option = 1;
     while (true) {
-      printOptions(); 
+      printOptions();
       try {
         option = Integer.parseInt(br.readLine());
       } catch (IOException e) {
         System.err.println("Invalid Format!");
       }
       switch (option) {
-        case  1: 
-          kette.print();
-          break;
-        case  2: 
-          kette.pop();
-          break;
-        case 3:
-          kette.pushCLI(br, kette);
-          break;
-        default: 
-          System.out.println("Invalid input");
+      case  1:
+        kette.print();
+        break;
+      case  2:
+        kette.pop();
+        break;
+      case 3:
+        kette.pushCLI(br, kette);
+        break;
+      default:
+        System.out.println("Invalid input");
       }
     }
-    
+
   }
 
   public Knoten pop() {
     if (kopf.successor != ende) {
       Knoten poppedKnoten = kopf.successor;
-      
+
       kopf.successor = poppedKnoten.successor;
       kopf.successor.predecessor = kopf;
-      
+
       System.out.println("Popped knoten with value " + poppedKnoten.data);
-      
+
       return poppedKnoten;
     } else {
       System.out.println("Error: Cant pop ende Knoten");
       return null;
     }
   }
-  
+
   public void push(Object data) {
     Knoten knoten = new Knoten(kopf, kopf.successor, data);
     kopf.successor.predecessor = knoten;
     kopf.successor = knoten;
-    
+
     System.out.println("Pushed knoten with value " + knoten.data);
   }
-  
+
   public void print() {
     Knoten knoten = kopf;
     int index = 1;
-    while (knoten != null) { 
+    while (knoten != null) {
       System.out.print(index + ": " + knoten.data + " ");
       index++;
       knoten = knoten.successor;
     }
     System.out.println();
   }
-  
+
   void pushCLI(BufferedReader br, Kette kette) {
     System.out.println("Enter the data of the element you want to push:");
     String data = "";
@@ -86,19 +86,19 @@ public class Kette {
     if (isInt) {
       kette.push(dataInt);
     } else {
-      kette.push(data);  
+      kette.push(data);
     }
-  }                                                  
-  
+  }
+
   static Kette initialize() {
     Kette kette = new Kette();
     kette.kopf.successor = kette.ende;
     return kette;
   }
-  
+
   static void printOptions() {
     System.out.println("Press [1] to print current chain");
     System.out.println("Press [2] to pop element");
-    System.out.println("Press [3] to push element"); 
+    System.out.println("Press [3] to push element");
   }
 }
